3a0d14de9ce191af48d7d1fa5d1013d44e56455e
sfwinters <sarah.f.winters@gmail.com>
2020-07-13 22:13:27 -0400

    added some features

--- js/game.js
+++ js/game.js
@@ -2,18 +2,29 @@ const oxford = 'https://od-api.oxforddictionaries.com/api/v2/entries/english/wor
 
 const submitWord = $('#submitWord');
 const randomWord = $('#randomWord');
-const btn = document.getElementsByClassName('button')
-const txt = document.getElementsByClassName('txt')
-h3 = document.getElementsByClassName('h3')
-console.log(btn)
+const btn = document.getElementsByClassName('button');
+const txt = document.getElementsByClassName('txt');
+const guessBox = $('#guessBox');
+const guessBtn = $('#guessBtn');
+const h3 = document.getElementsByClassName('h3');
+const wordSpaces = [];
 
 
 submitWord.click( e => {
     e.preventDefault();
-    const chosenWord = $('#guessWord').val();
-    renderWord(chosenWord);
+    const mysteryWord = $('#mysteryWord').val();
+    wordFactory(mysteryWord);
     hideElements();
     });
+
+guessBtn.click( e => {
+    e.preventDefault();
+    const guessedLetter = $('#guessBox').val();
+    console.log(guessedLetter);
+    if ($('#mysteryWord').val().includes(guessedLetter)) {
+        wordSpaces.splice(wordSpaces.indexOf[i], 1, guessedLetter)
+    };
+});
 
 
 // randomWord.on('click', e => {
@@ -41,18 +52,20 @@ submitWord.click( e => {
     function hideElements() {
         btn.submitWord.classList.add('hide-me');
         // btn.randomWord.classList.add('hide-me');
-        txt.guessWord.classList.add('hide-me');
+        txt.mysteryWord.classList.add('hide-me');
         // h3.h3.classList.add('hide-me')
+       guessBox.show();
+       guessBtn.show();
     }
 
-    function renderWord(data) {
-        let wordSpaces = '';
+    function wordFactory(data) {
         for (i = 0; i < data.length; i++) {
             if (i == 0) {
-                wordSpaces += '_';
+                wordSpaces.push('_');
             } else {
-                    wordSpaces += ' _';
+                wordSpaces.push(' _');
                 };
             };
-            document.body.append(wordSpaces)
+            $('.gameArea').append(wordSpaces);
         }
+    
