6b525edeaa237ce9993e280f29670a324b950b29
sfwinters <sarah.f.winters@gmail.com>
2020-07-16 20:56:00 -0400

    added hangman guy

--- js/game.js
+++ js/game.js
@@ -10,10 +10,15 @@ const h3 = document.getElementsByClassName('h3');
 let wordSpaces = [];
 let randomWordsArray = [];
 let finalWord = [];
-let winmsg = $('#winmsg');
+let winmsg = $('#winmsg')
 let losemsg = $('#losemsg');
+document.createElement('h2');
+let endmsg = document.getElementsByTagName('h2');
+endmsg.innerHTML = "The word was " + finalWord;
 winmsg.hide();
 losemsg.hide();
+let wrongGuess = 0;
+let imgloc = document.getElementsByClassName("hangimg");
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function wordFactory(word) {
@@ -34,7 +39,6 @@ submitWord.click( e => {
     wordFactory(mysteryWord);
     console.log(mysteryWord)
     finalWord.push(mysteryWord);
-    console.log(finalWord);
     });
 
 randomWord.on('click', e => {
@@ -51,8 +55,8 @@ randomWord.on('click', e => {
                 console.log(rando)
                 hideElements();
                 wordFactory(rando);
-                finalWord.push(rando)
-                console.log(finalWord)
+                finalWord.push(rando);
+                console.log(finalWord);
             };
             pickAWord();
         },
@@ -68,13 +72,12 @@ randomWord.on('click', e => {
 guessBtn.click( e => {
     e.preventDefault();
     let guessedLetter = $('#guessBox').val();
-    console.log(guessedLetter)
     let reallyFinal = finalWord[0];
-    let wrongGuess = 0;
     if (reallyFinal.includes(guessedLetter) === false) {
-        wrongGuess +=1;
+        wrongGuess += 1;
         guessesRemaining -=1;
-        console.log(wrongGuess);
+        
+        console.log(imgloc)
         } else {
             for (i = 0; i < reallyFinal.length; i++) {
                 if (reallyFinal[i] === guessedLetter) {
@@ -86,11 +89,9 @@ guessBtn.click( e => {
             }
         }
     $('.gameArea').html(wordSpaces.join('') + '</br>' + 'Guesses Remaining: ' + guessesRemaining);
+    $("#hangimg").attr("src","img/hangman-" + wrongGuess + ".png");
     guessBox.val('');
-    endGame();
-    console.log(wordSpaces);
-    console.log(reallyFinal);
-    console.log(wordSpaces.join(''))
+    endGame(); 
 });
     function hideElements() {
         btn.submitWord.classList.add('hide-me')
@@ -101,8 +102,8 @@ guessBtn.click( e => {
        guessBtn.show();
     } 
 
-    let wrongGuess = $('#guessBtn').wrongGuess
-    let guessesRemaining = 6
+    // let wrongGuess = $('#guessBtn').wrongGuess
+    let guessesRemaining = 7
     
     function endGame() {
         if (wordSpaces.join('') === finalWord[0]) {
@@ -114,12 +115,13 @@ guessBtn.click( e => {
             guessBtn.hide();
             guessBox.hide();
             losemsg.show();
+            $('.gameArea').html('The word was ' + finalWord + '.');
             $('#resetBtn').show();
         };
         $('#resetBtn').click( e => {
             e.preventDefault();
             location.reload();
-    })
+    });
     };
 
     const alphaDiv = $('.alphabet');
